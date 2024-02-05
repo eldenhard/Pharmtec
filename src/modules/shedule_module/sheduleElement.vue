0cbbb1929c12eb6bf1e17e02e83eacb299debcef
eldenhard <elden.hard@mail.ru>
2024-02-05 16:43:39 +0300

    создание, перетаскивание элменетов

--- src/modules/shedule_module/sheduleElement.vue
+++ src/modules/shedule_module/sheduleElement.vue
@@ -1,100 +1,129 @@
 <template>
     <div class="air_block" style="max-height: 90vh; overflow: hidden;">
-        <FullCalendar :options="calendarOptions" style="max-height: 70vh;">
-            <template v-slot:eventContent='arg'>
-                <b>{{ arg.timeText }}</b>
-                <i>{{ arg.event.title }}</i>
-            </template>
-        </FullCalendar>
+        <FullCalendar :options="calendarOptions" style="max-height: 70vh;" />
+        <buttonComponent @click="saveCurrentShedule()">Сохранить</buttonComponent>
     </div>
 </template>
-  
+
 <style scoped>
 @import './style/shedule.scss';
 </style>
 
 <script>
-import FullCalendar from '../../../node_modules/@fullcalendar/vue3'
-//   import resourceTimeGridPlugin from '../../../node_modules/@fullcalendar/resource-timegrid'
-import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid'
-import interactionPlugin from '../../../node_modules/@fullcalendar/interaction'
-import dayGridPlugin from '../../../node_modules/@fullcalendar/daygrid'
-import listPlugin from '../../../node_modules/@fullcalendar/list'
-import localeRu from '../../../node_modules/@fullcalendar/core/locales/ru'
-import { reactive } from 'vue'
+import buttonComponent from '../../ui/button/buttonComponent.vue';
+import FullCalendar from '../../../node_modules/@fullcalendar/vue3';
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
+import interactionPlugin from '../../../node_modules/@fullcalendar/interaction';
+import listPlugin from '../../../node_modules/@fullcalendar/list';
+import localeRu from '../../../node_modules/@fullcalendar/core/locales/ru';
+import { ref, reactive } from 'vue';
+import { useToast } from "vue-toastification";
 
 export default {
     components: {
-        FullCalendar
+        FullCalendar,
+        buttonComponent,
     },
     setup() {
-
-        const todayDate = new Date();
-        const dateString = todayDate.toJSON();
-        const dateStringSub = dateString.substring(0, dateString.indexOf("T"));
+        const toast = useToast();
+        // Хранение данных
+        const events = ref([]);
+        // Конфигурация
         const calendarOptions = reactive({
             schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
-            plugins: [resourceTimeGridPlugin],
-            
+            plugins: [resourceTimeGridPlugin, interactionPlugin, listPlugin],
             weekends: false,
+            //   Возможность выделения области
             selectable: true,
+            //   Начальный вид - график на 4 дня
             initialView: 'resourceTimeGridFourDay',
+            //   Верхняя панель инструментов
             headerToolbar: {
                 left: 'prev,next',
                 center: 'title',
-                right: 'resourceTimeGridDay,resourceTimeGridFourDay'
+                right: 'resourceTimeGridDay,resourceTimeGridFourDay',
             },
             views: {
                 resourceTimeGridFourDay: {
                     type: 'resourceTimeGrid',
                     duration: { days: 4 },
-                    buttonText: '4 дня'
+                    buttonText: '4 дня',
+                },
+            },
+            resources: [{ id: '1a', title: 'Переговорная комната' }],
+            select: (selectInfo) => {
+                const title = prompt('Введите название события');
+                if (title) {
+                    const newEvent = {
+                        title: title,
+                        start: selectInfo.startStr,
+                        end: selectInfo.endStr,
+                        resourceId: '1a',
+                    };
+                    events.value.push(newEvent);
                 }
             },
-            resources: [
-                { id: '1a', title: 'Переговорная комната' },
-            ],
+            eventAdd: (addInfo) => {
+                const title = prompt('Введите название события');
+                if (!title) {
+                    addInfo.event.remove();
+                } else {
+                    addInfo.event.setProp('title', title);
+                }
+            },
+            //   Обработчик перетаскивания события
+            eventDrop: (eventDropInfo) => {
+                const updateEvent = events.value.find((event) => event.id === eventDropInfo.event.id);
+                if (updateEvent && updateEvent.resourceId === '1a') {
+                    const title = prompt('Название');
+                    if (title) {
+                        const newEvent = {
+                            title: title,
+                            start: eventDropInfo.dateStr,
+                            end: eventDropInfo.dateStr,
+                            resourceId: '2a',
+                        };
+                        events.value.push(newEvent);
+                    }
+                }
 
+            },
+            events: events.value,
+            // Разбивка на часы и мин
             slotLabelFormat: {
                 hour: '2-digit',
                 minute: '2-digit',
                 omitZeroMinute: false,
                 meridiem: false,
             },
-            headerToolbar: {
-                center: 'addEventButton'
-            },
-            customButtons: {
-                addEventButton: {
-                    text: 'Добавить событие',
-                    click: function () {
-                        var dateStr = prompt('Enter a date in YYYY-MM-DD format');
-                        var date = new Date(dateStr + 'T00:00:00'); // will be in local time
-
-                        if (!isNaN(date.valueOf())) { // valid?
-                            calendar.addEvent({
-                                title: 'dynamic event',
-                                start: date,
-                                allDay: true
-                            });
-                            alert('Great. Now, update your database...');
-                        } else {
-                            alert('Invalid date.');
-                        }
-                    }
+            editable: true, // Включаем возможность редактирования событий
+            droppable: true, // Включаем возможность перетаскивания событий
+            //   Обработчик доабвления события при перетаскивании на календарь
+            eventReceive: (eventReceiveInfo) => {
+                const title = prompt('Введите название события');
+                if (title) {
+                    const newEvent = {
+                        title: title,
+                        start: eventReceiveInfo.dateStr,
+                        end: eventReceiveInfo.dateStr,
+                        resourceId: '1a',
+                    };
+                    events.value.push(newEvent);
                 }
             },
-
             locale: localeRu,
-
         });
-
+        const saveCurrentShedule = () => {
+            toast.warning('Раздел находится в разработке. Сохранение невозможно', {
+                    timeout: 3000
+                })
+        }
         return {
             calendarOptions,
+            events,
+            saveCurrentShedule,
+            toast,
         };
-    }
+    },
 };
 </script>
-
-
-
