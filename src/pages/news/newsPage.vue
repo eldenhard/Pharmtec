c650e9a55e91b40a7b4efa4fbcf93ba8dd537dab
[DAlexey <elden.hard@mail.ru>
2024-09-09 16:37:22 +0300

    раздел новостей

--- src/pages/news/newsPage.vue
+++ src/pages/news/newsPage.vue
@@ -2,8 +2,8 @@
     <div>
         <news_block />
         <div style="display: flex; justify-content: space-between; width: 80%; margin: 0 auto; align-items: center;">
-            <pagination_module />  
-            <a>Показать ещё</a>         
+            <pagination_module />
+            <a>Показать ещё</a>
         </div>
     </div>
 </template>
@@ -13,3 +13,11 @@
 import news_block from "./modules/news_block.vue"
 import pagination_module from "./modules/pagination_module.vue"
 </script>
+<style scoped>
+@media screen and (max-width: 768px) {
+
+    div>a {
+        display: none;
+    }
+}
+</style>
